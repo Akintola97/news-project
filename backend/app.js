04a7fc4a93344ad3d6d94f5839447be5b0b0f9c9
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2023-01-06 14:30:13 -0600

    update

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,21 @@
 var createError = require('http-errors');
 var express = require('express');
+var app = express();
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 var cors = require('cors');
+var bodyParser = require('body-parser')
 require('dotenv').config();
-
-
 var newsRouter = require('./routes/news');
 var articleDisplay = require('./routes/articleDisplay');
 
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.use('/', newsRouter)
