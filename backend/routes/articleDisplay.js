81083a62c31c7d34a42e8d758e872a922c27936e
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2023-01-06 19:43:31 -0600

    update

--- backend/routes/articleDisplay.js
+++ backend/routes/articleDisplay.js
@@ -7,13 +7,14 @@ const apiKey = process.env.API_KEY
 
 
 
-articleRouter.get('/:title', async(req, res)=> {
-    const articleTitle = req.params.title
-    const url = `https://newsapi.org/v2/everything?q=playstation&title=${articleTitle}&apiKey=${apiKey}`
+articleRouter.get('/:url', async(req, res)=> {
+    const articleUrl = req.params.url
+    const url = `https://newsapi.org/v2/everything?q=${url}&searchIn=url&apiKey=${apiKey}`
     try {
         const newsApi = await axios.get(url)
-        res.render('articleDisplay', {display: newsApi.data})
-       
+        res.render('articleDisplay', {display: newsApi.data.articles[0]})
+        console.log("API CALL")
+        console.log(newsApi.data)
     }   
     catch(error){
        console.log(error)
